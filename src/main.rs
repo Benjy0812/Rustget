0401e0b9e84ced1f52ccbe865df1229ca906ed96
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-07-20 21:25:37 +0200

    Fix many errors first time using clap

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,52 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Rustget")]
+#[command(version = "0.1")]
+#[command(about = "package manager written in Rust", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install { name: String },
+    Remove { name: String },
+    Search { term: String },
+    List,
+    Info,
+}
+
 fn main() {
     println!("Welcome to Rustget!");
-    println!("Chose (1, 2, 3, 4)");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Install { name } => install(name),
+        Commands::Remove { name } => uninstall(name),
+        Commands::Search { term } => search(term),
+        Commands::List => list(),
+        Commands::Info => info(),
+    }
 }
 
-enum Commands {
-    Isntall(String),
-    Remove(String),
-    Search(String),
-    List,
+fn install(name: String) {
+    println!("Installing {name}");
 }
 
-fn install() {}
+fn uninstall(name: String) {
+    println!("Uninstalling {name}");
+}
 
-fn listInstalled() {}
+fn list() {
+    println!("Listing Isntalled");
+}
 
-fn uninstall() {}
+fn search(term: String) {
+    println!("Search {term}");
+}
 
-fn search() {}
+fn info() {
+    println!("Info");
+}
