e473c9675539c358cb30b2412f166cf7a734f721
Benjy <108727379+Benjy0812@users.noreply.github.com>
2026-07-28 01:11:30 +0200

    wip: add struct for manifests and add loader

---
+++ src/database.rs
@@ -0,0 +1,25 @@
+pub use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Database {
+    pub category: String,
+    pub index: Vec<String>,
+    pub packages: Vec<Package>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub description: String,
+    pub download_url: String,
+    pub sha256: String,
+}
+
+pub fn load_database(path: &str) -> Result<Database, Box<dyn std::error::Error>> {
+    let json = std::fs::read_to_string(path)?;
+
+    let database: Database = serde_json::from_str(&json)?;
+
+    Ok(Database)
+}
